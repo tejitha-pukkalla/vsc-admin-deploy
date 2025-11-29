64c48e3110dd097aa885d487e56188b50069634e
Tejitha Pukkalla <tejithapukkalla@gmail.com>
2025-11-29 13:38:32 +0530

    api fix

--- src/services/api.js
+++ src/services/api.js
@@ -69,7 +69,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL + '/api' || 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
